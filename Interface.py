a29c42d14688c4f7d2c42458862f145333a28b8d
artuzao gamer <artuzao97@gmail.com>
2023-04-07 23:45:42 -0300

    Created UI variables

--- Interface.py
+++ Interface.py
@@ -7,6 +7,10 @@ def main(page: ft.Page):
     page.window_width = 816
     page.padding = 0
     
+    def generate(e):
+        cc_container.visible=True
+        page.update()
+    
     page.add(ft.Row(alignment=ft.MainAxisAlignment.CENTER, controls=[ft.Container(margin=0, bgcolor='#ECF0F1', width=800, height=600, content=ft.Row(spacing=0, controls=[ft.Container(
         width=260, height=600, padding=ft.padding.only(top=30, left=30, right=30), bgcolor='#ECF0F1', border=ft.border.all(2, '#7F8C8D'), border_radius=ft.border_radius.only(topRight=30, bottomRight=30), content=ft.Column(spacing=10, alignment='start', horizontal_alignment='center', controls=[
             ft.Text("DEN'S GENERATOR", color='#D35400', size=18, weight=ft.FontWeight.W_500),
@@ -18,13 +22,13 @@ def main(page: ft.Page):
                                  ft.dropdown.Option("AMERICAN EXPRESS")]),
             ft.TextField(value='5', prefix_text='Amount: ', focused_border_color='#D35400', filled=True, bgcolor='#D9D9D9', prefix_style=ft.TextStyle(color='#D35400', size=12), width=180, height=40, border_radius=20, border_color='#e67e22', content_padding=ft.padding.only(left=10, right=10), text_style=ft.TextStyle(color='#D35400', size=12)),
             ft.Divider(height=10, opacity=0),
-            ft.ElevatedButton(text='Generate', style=ft.ButtonStyle(bgcolor='#D35400', color='#ECF0F1', overlay_color=ft.colors.ORANGE_900), width=180, height=40)
+            ft.ElevatedButton(text='Generate', style=ft.ButtonStyle(bgcolor='#D35400', color='#ECF0F1', overlay_color=ft.colors.ORANGE_900), width=180, height=40, on_click=generate)
         ])),
                                                                                                                                             ft.Container(padding=ft.padding.only(top=30, left=30, right=30), bgcolor='#ECF0F1', width=540, height=600, content=ft.Column(spacing=12, horizontal_alignment='center', controls=[
                                                                                                                                                 ft.Text('RESULTS', color='#D35400', size=18, weight=ft.FontWeight.W_500),
                                                                                                                                                 ft.Divider(height=0),
                                                                                                                                                 ft.Divider(height=143, opacity=0),
-                                                                                                                                                ft.Row(spacing=5, alignment='center', controls=[
+                                                                                                                                                ft.Row(spacing=5, visible=False, alignment='center', controls=[
                                                                                                                                                     ft.IconButton(icon=ft.icons.ARROW_LEFT, icon_size=50, scale=1),
                                                                                                                                                     ft.Container(padding=ft.padding.all(15), width=225, height=165, border_radius=20, gradient=ft.LinearGradient(colors=['#2C3E50', '#34495E'], rotation=-90), content=ft.Column(controls=[
                                                                                                                                                         ft.Text(value='VISA', weight=ft.FontWeight.BOLD, size=16),
@@ -42,6 +46,15 @@ def main(page: ft.Page):
                                                                                                                                                     ])
                                                                                                                                             ]))])
     )]))
+    generator_container = page.controls[0].controls[0].content.controls[0].content
+    generator_amount = generator_container.controls[4]
+    generator_brand = generator_container.controls[3]
+    cc_container = page.controls[0].controls[0].content.controls[1].content.controls[3]
+    cc_brand = cc_container.controls[1].content.controls[0]
+    cc_number = cc_container.controls[1].content.controls[2].controls[0]
+    cc_exp = cc_container.controls[1].content.controls[2].controls[1]
+    cc_holder = cc_container.controls[1].content.controls[3].controls[0]
+    cc_cvv = cc_container.controls[1].content.controls[3].controls[1]
 
 if __name__ == '__main__':
     ft.app(target=main)
